6900de7e39210fc54a9dd1f96cfee2c7c52e0be6
Gnosis <emmagab38@gmail.com>
2025-06-19 08:19:35 +0100

    commit changes

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -393,74 +393,74 @@ Our developers put special focus on making sure your account is easy to manage a
 
             <!-- ticker-area -->
             <!-- TradingView Widget BEGIN -->
-<div class="tradingview-widget-container">
-  <div class="tradingview-widget-container__widget"></div>
-  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"></a></div>
-  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
-  {
-  "symbols": [
-    {
-      "proName": "FOREXCOM:SPXUSD",
-      "title": "S&P 500 Index"
-    },
-    {
-      "proName": "FOREXCOM:NSXUSD",
-      "title": "US 100 Cash CFD"
-    },
-    {
-      "proName": "FX_IDC:EURUSD",
-      "title": "EUR to USD"
-    },
-    {
-      "proName": "BITSTAMP:BTCUSD",
-      "title": "Bitcoin"
-    },
-    {
-      "proName": "BITSTAMP:ETHUSD",
-      "title": "Ethereum"
-    },
-    {
-      "description": "",
-      "proName": "NASDAQ:TSLA"
-    },
-    {
-      "description": "",
-      "proName": "NASDAQ:NVDA"
-    },
-    {
-      "description": "",
-      "proName": "NASDAQ:AAPL"
-    },
-    {
-      "description": "",
-      "proName": "NASDAQ:MSFT"
-    },
-    {
-      "description": "",
-      "proName": "NASDAQ:META"
-    },
-    {
-      "description": "",
-      "proName": "COINBASE:SOLUSD"
-    },
-    {
-      "description": "",
-      "proName": "BITSTAMP:XRPUSD"
-    },
-    {
-      "description": "",
-      "proName": "BINANCE:BNBUSDT"
-    }
-  ],
-  "showSymbolLogo": true,
-  "isTransparent": false,
-  "displayMode": "adaptive",
-  "colorTheme": "dark",
-  "locale": "en"
-}
-  </script>
-</div>
-<!-- TradingView Widget END -->
+            <div class="tradingview-widget-container">
+              <div class="tradingview-widget-container__widget"></div>
+              <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"></a></div>
+              <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
+              {
+              "symbols": [
+                {
+                  "proName": "FOREXCOM:SPXUSD",
+                  "title": "S&P 500 Index"
+                },
+                {
+                  "proName": "FOREXCOM:NSXUSD",
+                  "title": "US 100 Cash CFD"
+                },
+                {
+                  "proName": "FX_IDC:EURUSD",
+                  "title": "EUR to USD"
+                },
+                {
+                  "proName": "BITSTAMP:BTCUSD",
+                  "title": "Bitcoin"
+                },
+                {
+                  "proName": "BITSTAMP:ETHUSD",
+                  "title": "Ethereum"
+                },
+                {
+                  "description": "",
+                  "proName": "NASDAQ:TSLA"
+                },
+                {
+                  "description": "",
+                  "proName": "NASDAQ:NVDA"
+                },
+                {
+                  "description": "",
+                  "proName": "NASDAQ:AAPL"
+                },
+                {
+                  "description": "",
+                  "proName": "NASDAQ:MSFT"
+                },
+                {
+                  "description": "",
+                  "proName": "NASDAQ:META"
+                },
+                {
+                  "description": "",
+                  "proName": "COINBASE:SOLUSD"
+                },
+                {
+                  "description": "",
+                  "proName": "BITSTAMP:XRPUSD"
+                },
+                {
+                  "description": "",
+                  "proName": "BINANCE:BNBUSDT"
+                }
+              ],
+              "showSymbolLogo": true,
+              "isTransparent": false,
+              "displayMode": "adaptive",
+              "colorTheme": "dark",
+              "locale": "en"
+            }
+              </script>
+            </div>
+            <!-- TradingView Widget END -->
 
             <!-- ticker-area-end -->
 
--- resources/views/pages/layout/app.blade.php
+++ resources/views/pages/layout/app.blade.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Ironik - ICO & Crypto Landing Page Template</title>
+    <title>{{ env('APP_NAME') }}</title>
     <meta name="description" content="Ironik - ICO & Crypto Landing Page Template">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
